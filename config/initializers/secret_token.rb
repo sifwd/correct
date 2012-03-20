641557de13a221ed0555f4ba7f212a0c73f81cca
Simon Clark <simon@forward.co.uk>
2012-03-20 22:45:03 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corretc::Application.config.secret_token = 'a03b2143ee7e71cfbe958fc4baa39e88e333039c870cf6a85dae64230dabfdb14c1b6721f58778762e4f97a255ed4a71c9fad83ed93d41d17b7f39f6c5c1f85b'
