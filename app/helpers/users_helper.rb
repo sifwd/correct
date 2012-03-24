d6b8a6ad17e437388635d00d7e9d2ad035c4772c
Simon Clark <simon@forward.co.uk>
2012-03-24 16:14:14 +0000

    tweaks

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png"
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
