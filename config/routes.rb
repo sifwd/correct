44c8cbb3af7260c13206c5a946e313588c5cc5b5
Simon Clark <simon@forward.co.uk>
2012-03-24 12:58:55 +0000

    Added signup controller and fixed layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Corretc::Application.routes.draw do
+  get "users/new"
+
   root to:  'static_pages#home'
   match '/about', to: 'static_pages#about'
-  
+  match '/signup', to: 'users#new'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
