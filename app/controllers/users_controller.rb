4058d66552ef100e9225949fc51baf164297223c
Simon Clark <simon@forward.co.uk>
2012-03-24 15:56:28 +0000

    User signup completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # success!
+       flash[:success] = "Signup Complete. Welcome to the App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Corretc::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to:  'static_pages#home'
   match '/about', to: 'static_pages#about'
